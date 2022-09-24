b011f02961bd482c45597b1f4b2a9f9d953ee538
Kyu Lee <kwooler@gmail.com>
2022-09-24 15:00:06 -0700

    A65

--- A65.hpp
+++ A65.hpp
@@ -4,23 +4,5 @@
 #include <iostream>
 using namespace std;
 
-void swap(int &, int &, int &);
-void swaptwo(int &, int &);
 // complete two functions
-void getinput(int &num1, int &num2, int &num3)
-{
-        cout << "Enter your input ( 3 integers )\n";
-        cin >> num1 >> num2 >> num3;
-}
-void swaptwo(int &i, int &j)
-{
-        int tmp;
-        tmp = i;
-        i = j;
-        j = tmp;
-}
-void swap(int &num1, int &num2, int &num3)
-{
-        swaptwo(num1, num2);
-        swaptwo(num1, num3);
-}
+// swap() and swaptwo()
